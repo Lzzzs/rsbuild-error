fba9cb66fcd22cb5f5cf19fcdf69d5a0f9689a91
Lzzzs <1250564179@qq.com>
2023-11-16 10:41:34 +0800

    -

---
+++ rsbuild.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+const path = require('path');
+
+const port = 8080;
+const resolve = dir => path.join(__dirname, dir);
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  dev: {
+    port,
+    https: true,
+    historyApiFallback: true,
+  },
+  source: {
+    alias: {
+      '@': resolve('src/')
+    },
+  },
+  html: {
+    template: './public/index.html',
+  },
+  output: {
+    distPath: {
+      html: '',
+    },
+    externals: {
+      "react": "React",
+      "react-dom": "ReactDOM",
+      "exceljs": "ExcelJS",
+      "wangeditor": "wangEditor",
+      "html2canvas": "html2canvas"
+    },
+    disableSourceMap: false,
+  },
+  performance: {
+    chunkSplit: {
+      strategy: 'custom',
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: 'initial',
+            minChunks: 2,
+            maxInitialSize: 5,
+            minSize: 0
+          },
+          vendor: {
+            test: /node_modules/,
+            chunks: 'initial',
+            name: 'vendor',
+            priority: 10,
+            enforce: true
+          }
+        },
+      },
+    },
+    removeConsole: ['log', 'warn'],
+  },
+});
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import { WangEditor } from './components/editor';
+
+interface IProps {
+
+}
+
+interface IState {
+  data: string;
+}
+
+export default class App extends React.Component<IProps, IState> {
+  private editorRef: any = React.createRef();
+
+  readonly state = {
+    data: ''
+  } as IState
+
+  public render() {
+    return (
+      <div className="App">
+        <WangEditor
+          ref={this.editorRef}
+          defaultValue={this.state.data ?? ''}
+          onChange={(html) => {
+            this.setState({ data: html });
+          }}
+        />
+      </div>
+    );
+  }
+}
